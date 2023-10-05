6ad1a384e6c719f9cc30e7ce6ac0516d93d42d2b
kangkong05 <kangkong@vendit.co.kr>
2023-10-05 18:59:46 +0900

    버그 고치고 만다

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { chromium, devices } from 'playwright';
     const browser = await chromium.launch({headless: false});
     const context = await browser.newContext(devices['Desktop Chrome']);
     const page = await context.newPage()
-    
+
     // The actual interesting bit
     await context.route('**.jpg', route => route.abort())
     await page.goto('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100')
@@ -17,8 +17,13 @@ import { chromium, devices } from 'playwright';
     for(let entrie of entries) {
         const obj:any = {}
 
-        const articleTitle = await(await entrie.$(`div.sh_text > a`))?.innerText()
-        obj.articleTitle = articleTitle
+        const title = await(await entrie.$(`div.sh_text > a`))?.innerText()
+        const content = await(await entrie.$(`div.sh_text > div.sh_text_lede`))?.innerText()
+        const company = await(await entrie.$(`div.sh_text > div.sh_text_info > div.sh_text_press`))?.innerText()
+
+        obj.title = title
+        obj.content = content
+        obj.company = company
 
         list.push(obj)
     }
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,22 +1,74 @@
-import { test, devices, expect } from '@playwright/test';
+import { devices, test } from '@playwright/test';
 
 test.use(devices['Desktop Chrome'])
 
 test('naver news crawling', async({page, context}) => {
-    await context.route('**.jpg', route => route.abort())
     await page.goto('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100')
 
-    const entries = await page.$$(`.section_headline > ul > li`)
-
+    const entries = await page.locator(`.section_headline > ul > li`).all()
     let list:any = []
 
-    for(let entrie of entries) {
-        const obj:any = {}
+    console.log(entries)
 
-        const articleTitle = await(await entrie.$(`div.sh_text > a`))?.innerText()
-        obj.articleTitle = articleTitle
+    entries.map(async (entrie) => {
+        console.log(entrie)
+        let obj:any = {}
+
+        const title = await entrie.allInnerTexts
+        const content = await entrie.locator(`div.sh_text > div.sh_text_lede`)?.innerText()
+        const company = await entrie.locator(`div.sh_text > div.sh_text_info > div.sh_text_press`)?.innerText()
+
+        obj.title = title
+        obj.content = content
+        obj.company = company
 
         list.push(obj)
-    }
+    })
+
     console.log(list)
+
+
+
+    // console.log(await Promise.all(entries).)
+    // console.log(await Promise.all(entries))
+
+    // for (const li of await Promise.all(entries))
+
+    // Promise.all(entries)
+
+    // onClick = async () => {
+    //     console.time("ABC");
+    //     try {
+    //       const data001 = new Promise((resolve) =>
+    //         setTimeout(resolve, 3000, "promise001")
+    //       );
+      
+    //       const data002 = Promise.reject("promise002 reject");
+      
+    //       const data003 = await Promise.all([data001, data002]);
+    //       console.log(data003);
+    //     } catch (error) {
+    //       console.error(error);
+    //     } finally {
+    //       console.timeEnd("ABC");
+    //     }
+    //   };
+
+    // for(let entrie of entries) {
+    //     const obj:any = {}
+        
+    //     const title = await(await entrie.locator(`div.sh_text > a`))?.innerText()
+    //     const content = await(await entrie.locator(`div.sh_text > div.sh_text_lede`))?.innerText()
+    //     const company = await(await entrie.$(`div.sh_text > div.sh_text_info > div.sh_text_press`))?.innerText()
+
+    //     obj.title = title
+    //     obj.content = content
+    //     obj.company = company
+
+    //     list.push(obj)
+
+    //     break
+    // }
+
+    // console.log(list)
 })
