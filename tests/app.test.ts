91205ec4a9237279c3d35938d7bf3355b3c1f195
kangkong05 <kangkong@vendit.co.kr>
2023-10-06 17:13:25 +0900

    ✅ :: update crawler test
    - 어제 못 고친 에러 해결

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -5,70 +5,20 @@ test.use(devices['Desktop Chrome'])
 test('naver news crawling', async({page, context}) => {
     await page.goto('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100')
 
-    const entries = await page.locator(`.section_headline > ul > li`).all()
-    let list:any = []
+    const entries = await page.locator('.section_headline > ul > li').all()
 
-    console.log(entries)
+    let list:Array<Object> = []
 
-    entries.map(async (entrie) => {
-        console.log(entrie)
-        let obj:any = {}
+    const promise = entries.map(async (entrie) => {
+        const title = await entrie.locator('.sh_text > a')?.innerText()
+        const content = await entrie.locator('.sh_text > .sh_text_lede')?.innerText()
+        const company = await entrie.locator('.sh_text .sh_text_press')?.innerText()
+        const thumbnail = await entrie.locator('.sh_thumb_link > img').getAttribute('src')
 
-        const title = await entrie.allInnerTexts
-        const content = await entrie.locator(`div.sh_text > div.sh_text_lede`)?.innerText()
-        const company = await entrie.locator(`div.sh_text > div.sh_text_info > div.sh_text_press`)?.innerText()
-
-        obj.title = title
-        obj.content = content
-        obj.company = company
-
-        list.push(obj)
+        list.push({title, content, company, thumbnail})
     })
 
+    await Promise.all(promise)
+
     console.log(list)
-
-
-
-    // console.log(await Promise.all(entries).)
-    // console.log(await Promise.all(entries))
-
-    // for (const li of await Promise.all(entries))
-
-    // Promise.all(entries)
-
-    // onClick = async () => {
-    //     console.time("ABC");
-    //     try {
-    //       const data001 = new Promise((resolve) =>
-    //         setTimeout(resolve, 3000, "promise001")
-    //       );
-      
-    //       const data002 = Promise.reject("promise002 reject");
-      
-    //       const data003 = await Promise.all([data001, data002]);
-    //       console.log(data003);
-    //     } catch (error) {
-    //       console.error(error);
-    //     } finally {
-    //       console.timeEnd("ABC");
-    //     }
-    //   };
-
-    // for(let entrie of entries) {
-    //     const obj:any = {}
-        
-    //     const title = await(await entrie.locator(`div.sh_text > a`))?.innerText()
-    //     const content = await(await entrie.locator(`div.sh_text > div.sh_text_lede`))?.innerText()
-    //     const company = await(await entrie.$(`div.sh_text > div.sh_text_info > div.sh_text_press`))?.innerText()
-
-    //     obj.title = title
-    //     obj.content = content
-    //     obj.company = company
-
-    //     list.push(obj)
-
-    //     break
-    // }
-
-    // console.log(list)
 })
