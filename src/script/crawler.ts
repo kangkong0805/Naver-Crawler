a5e45b8249ab1f35e229667b0d028ba3e445e478
kangkong05 <kangkong@vendit.co.kr>
2023-11-09 16:01:39 +0900

    Update: browser headless false

--- src/script/crawler.ts
+++ src/script/crawler.ts
@@ -2,7 +2,7 @@ import { Page, chromium } from "playwright";
 import { dailyHotel, ddnayo, yanolja, yeogieottae } from "../test/index";
 
 const crawling = async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
 
   const otaCrawler = async <T>(func: (page: Page) => T) => {
