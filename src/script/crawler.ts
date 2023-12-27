879fa30423a84913bb6c8cacc7f2eda99becac2a
kangkong0805 <kang3530554@gmail.com>
2023-12-27 17:03:07 +0900

    Add: test console

--- src/script/crawler.ts
+++ src/script/crawler.ts
@@ -2,6 +2,7 @@ import { Page, chromium } from "playwright";
 import { dailyHotel, ddnayo, yanolja, yeogieottae } from "../test/index";
 
 const crawling = async () => {
+  console.log('test console')
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
 
