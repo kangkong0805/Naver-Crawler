26ea1dcdf36ae0dbb7d692f8f68a6505aba6a215
kangkong0805 <kang3530554@gmail.com>
2023-12-27 18:47:46 +0900

    Add: 웹페이지에서 버튼 누르면 크롤링 되게 하려는 의도지만 잘 안되네

--- src/script/crawler.ts
+++ src/script/crawler.ts
@@ -2,7 +2,6 @@ import { Page, chromium } from "playwright";
 import { dailyHotel, ddnayo, yanolja, yeogieottae } from "../test/index";
 
 const crawling = async () => {
-  console.log('test console')
   const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
 
