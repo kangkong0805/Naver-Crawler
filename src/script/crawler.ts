c804eb95fc6e6295a04f52daf0de737b7b279a71
kangkong0805 <kang3530554@gmail.com>
2023-12-27 18:05:13 +0900

    Fix: headless

--- src/script/crawler.ts
+++ src/script/crawler.ts
@@ -3,7 +3,7 @@ import { dailyHotel, ddnayo, yanolja, yeogieottae } from "../test/index";
 
 const crawling = async () => {
   console.log('test console')
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
 
   const otaCrawler = async <T>(func: (page: Page) => T) => {
